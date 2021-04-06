4064a90d021b0b4ac4e672f013492209b2580d8f
zhangzhonghao <zhangzhonghao@example.com>
2021-04-06 09:45:39 +0800

    add ButterKnife

--- app/src/main/java/com/nfh/androiddemo/BaseActivity.java
+++ app/src/main/java/com/nfh/androiddemo/BaseActivity.java
@@ -5,10 +5,24 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 public class BaseActivity extends AppCompatActivity {
+
+    Unbinder unbinder;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        unbinder = ButterKnife.bind(this);
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (unbinder != null) {
+            unbinder.unbind();
+        }
     }
 }
--- app/src/main/java/com/nfh/androiddemo/MainActivity.java
+++ app/src/main/java/com/nfh/androiddemo/MainActivity.java
@@ -2,8 +2,14 @@ package com.nfh.androiddemo;
 
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
 
 public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.btn_beidou)
+    Button btnBeidou;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
